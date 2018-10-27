b7b0166324fd8921069486265b51055bea18a69a
Frank Blechschmidt <contact@frank-blechschmidt.com>
2018-10-28 01:34:13 +0200

    Remove test Java app

--- java/src/main/java/App.java
+++
@@ -1,8 +0,0 @@
-import edu.stanford.nlp.python.SUTimeWrapper;
-
-public class App {
-    public static void main(String[] args) {
-        SUTimeWrapper wrapper = new SUTimeWrapper(false, false, "spanish");
-        System.out.println(wrapper.annotate("Hoy he escrito una prueba."));
-    }
-}
