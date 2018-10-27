9981e6c968c6d04ae55aa19067ab656ff62dbdc2
Frank Blechschmidt <frank.blechschmidt@sap.com>
2018-10-27 15:44:04 +0200

    Run black formatter on sutime folder

--- sutime/sutime.py
+++ sutime/sutime.py
@@ -31,14 +31,20 @@ class SUTime(object):
     """
 
     _required_jars = {
-        'stanford-corenlp-3.9.2-models.jar',
-        'stanford-corenlp-3.9.2.jar',
-        'gson-2.8.5.jar',
-        'slf4j-simple-1.7.25.jar'
+        "stanford-corenlp-3.9.2-models.jar",
+        "stanford-corenlp-3.9.2.jar",
+        "gson-2.8.5.jar",
+        "slf4j-simple-1.7.25.jar",
     }
 
-    def __init__(self, jars=None, jvm_started=False, mark_time_ranges=False, include_range=False,
-                 jvm_flags=None):
+    def __init__(
+        self,
+        jars=None,
+        jvm_started=False,
+        mark_time_ranges=False,
+        include_range=False,
+        jvm_flags=None,
+    ):
         """Initializes SUTime.
         """
         self.mark_time_ranges = mark_time_ranges
@@ -59,42 +65,44 @@ class SUTime(object):
             self._lock.acquire()
 
             SUTimeWrapper = jpype.JClass(
-                'edu.stanford.nlp.python.SUTimeWrapper')
+                "edu.stanford.nlp.python.SUTimeWrapper"
+            )
             self._sutime = SUTimeWrapper(
-                self.mark_time_ranges, self.include_range)
+                self.mark_time_ranges, self.include_range
+            )
             self._is_loaded = True
         finally:
             self._lock.release()
 
     def _start_jvm(self, additional_flags):
         if jpype.isJVMStarted() is not 1:
-            flags = ['-Djava.class.path=' + self._classpath,]
+            flags = ["-Djava.class.path=" + self._classpath]
             if additional_flags:
                 flags.extend(additional_flags)
-            jpype.startJVM(
-                jpype.getDefaultJVMPath(),
-                *flags
-            )
+            jpype.startJVM(jpype.getDefaultJVMPath(), *flags)
 
     def _create_classpath(self):
-        sutime_jar = os.path.join(*[
-            imp.find_module('sutime')[1],
-            'jars',
-            'stanford-corenlp-sutime-python-1.0.0.jar'
-        ])
+        sutime_jar = os.path.join(
+            *[
+                imp.find_module("sutime")[1],
+                "jars",
+                "stanford-corenlp-sutime-python-1.0.0.jar",
+            ]
+        )
         jars = [sutime_jar]
         jar_file_names = []
         for top, dirs, files in os.walk(self.jars):
             for file_name in files:
-                if file_name.endswith('.jar'):
+                if file_name.endswith(".jar"):
                     jars.append(os.path.join(top, file_name))
                     jar_file_names.append(file_name)
         if not SUTime._required_jars.issubset(jar_file_names):
             raise RuntimeError(
-                'Not all necessary Java dependencies have been downloaded!')
+                "Not all necessary Java dependencies have been downloaded!"
+            )
         return os.pathsep.join(jars)
 
-    def parse(self, input_str, reference_date=''):
+    def parse(self, input_str, reference_date=""):
         """Parses datetime information out of string input.
 
         It invokes the SUTimeWrapper.annotate() function in Java.
@@ -111,7 +119,7 @@ class SUTime(object):
             RuntimeError: An error occurres when CoreNLP is not loaded.
         """
         if self._is_loaded is False:
-            raise RuntimeError('Please load SUTime first!')
+            raise RuntimeError("Please load SUTime first!")
 
         if reference_date:
             return json.loads(self._sutime.annotate(input_str, reference_date))
